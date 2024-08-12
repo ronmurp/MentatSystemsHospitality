788daaa0ce3984700c4077cf20b10fe55a20a402
Ron Murphy <ronmurp@gmail.com>
2024-08-12 23:07:01 +0100

    Pay.CoinCorner - Add project and config

---
+++ solution/Msh/Msh.Pay.CoinCorner/Models/CoinCornerConfig.cs
@@ -0,0 +1,29 @@
+﻿using Msh.Common.Models.Configuration;
+
+namespace Msh.Pay.CoinCorner.Models;
+
+/// <summary>
+/// All must be lower case.
+/// </summary>
+public class CoinCornerConfig
+{
+    public string UserId { get; set; }
+    public string PublicKey { get; set; }
+
+    public ConfigSecret Secret { get; set; } = new ConfigSecret();
+
+    /// <summary>
+    /// No trailing /
+    /// </summary>
+    public string CheckoutUrl { get; set; } = "https://checkout.CoinCorner.com/api";
+
+    public string SuccessRedirectUrl { get; set; }
+
+    public string FailRedirectUrl { get; set; }
+
+    public string NotificationUrl { get; set; }
+
+    public bool LogEnable { get; set; }
+
+    public string LogFilePath { get; set; }
+}
