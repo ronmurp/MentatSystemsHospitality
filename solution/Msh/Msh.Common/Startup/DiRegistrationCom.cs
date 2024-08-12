8ac90582a0e102e566a381725843a3b1e45375f8
Ron Murphy <ronmurp@gmail.com>
2024-08-12 20:32:43 +0100

    Common - Add service registration

---
+++ solution/Msh/Msh.Common/Startup/DiRegistrationCom.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Msh.Common.Data;
+
+namespace Msh.Common.Startup;
+
+public static class DiRegistrationCom
+{
+    public static void RegisterGpServices(this IServiceCollection services)
+    {
+        services.AddScoped<IConfigRepository, ConfigRepository>();
+        services.AddScoped<ConfigDbContext, ConfigDbContext>();
+    }
+}
