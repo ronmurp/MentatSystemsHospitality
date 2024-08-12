c722bef557e9d357848d026d7248209dc455959f
Ron Murphy <ronmurp@gmail.com>
2024-08-12 21:17:09 +0100

    WebApp - Add project

---
+++ solution/Msh/Msh.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Msh.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
