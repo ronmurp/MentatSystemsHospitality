4d19d8bb8edf245da86e105a7564db9bf7a46607
Ron Murphy <ronmurp@gmail.com>
2024-08-12 20:29:36 +0100

    Common - Add NullConfigException

---
+++ solution/Msh/Msh.Common/Exceptions/NullConfigException.cs
@@ -0,0 +1,7 @@
+﻿namespace Msh.Common.Exceptions;
+
+/// <summary>
+/// An exception thrown when an expected Config does not exist in the db
+/// </summary>
+/// <param name="message"></param>
+public class NullConfigException(string message) : Exception(message);
