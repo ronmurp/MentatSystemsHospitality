ce2874a129d7eb7567ff0d071d253f1d1dddb610
Ron Murphy <ronmurp@gmail.com>
2024-08-12 22:02:24 +0100

    WebApp - Add DiConfiguration

---
+++ solution/Msh/Msh.WebApp/Startup/DiConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Msh.Common.Startup;
+
+namespace Msh.WebApp.Startup;
+
+public static class DiConfiguration
+{
+    public static void LoadServices(this WebApplicationBuilder builder)
+    {
+        var services = builder.Services;
+
+        services.RegisterCommonServices();
+    }
+}
