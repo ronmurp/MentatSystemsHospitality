8fc4940d991b51648317b648fb82906c68feb266
Ron Murphy <ronmurp@gmail.com>
2024-08-13 17:56:29 +0100

    Pay.FreedomPay - Add ConstFp

---
+++ solution/Msh/Msh.Pay.FreedomPay/Models/ConstFp.cs
@@ -0,0 +1,7 @@
+﻿namespace Msh.Pay.FreedomPay.Models;
+
+public static class ConstFp
+{
+    public const string FpErrorCodeBank = "FpErrorCodeBank";
+    public const string FpErrorCode = "FpErrorCode";
+}
