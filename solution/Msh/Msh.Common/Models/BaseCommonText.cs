728ddc79d937faff3a871186128c28ecb31f7045
Ron Murphy <ronmurp@gmail.com>
2024-08-13 22:37:54 +0100

    Common - Add CommonTexts

---
+++ solution/Msh/Msh.Common/Models/BaseCommonText.cs
@@ -0,0 +1,10 @@
+﻿namespace Msh.Common.Models;
+
+/// <summary>
+/// A common text element
+/// </summary>
+public class BaseCommonText
+{
+    public string Id { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+}
---
+++ solution/Msh/Msh.Common/Models/CommonText.cs
@@ -0,0 +1,10 @@
+﻿namespace Msh.Common.Models;
+
+/// <summary>
+/// A common text element
+/// </summary>
+public class CommonText : BaseCommonText
+{
+    public DateTime StartDate { get; set; } = DateTime.MinValue;
+    public DateTime EndDate { get; set; } = DateTime.MaxValue;
+}
