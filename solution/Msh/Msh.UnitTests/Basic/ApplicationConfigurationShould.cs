88ac280a2895bb571e1d2ee0039340eab572abd8
Ron Murphy <ronmurp@gmail.com>
2024-08-13 15:25:52 +0100

    UnitTests - Add project - Test Config operations

---
+++ solution/Msh/Msh.UnitTests/Basic/ApplicationConfigurationShould.cs
@@ -0,0 +1,21 @@
+﻿using Msh.WebApp.Models;
+using NUnit.Framework;
+
+namespace Msh.UnitTests.Basic;
+
+[TestFixture]
+public class ApplicationConfigurationShould
+{
+    /// <summary>
+    /// Test that the ApplicationConfiguration returns appropriate strings
+    /// </summary>
+    /// <param name="key"></param>
+    [Test]
+    [TestCase("ConnectionStrings:DefaultConnection")]
+    public void ReturnCorrectAppSettingsValue(string key)
+    {
+        var value = ApplicationConfiguration.GetSetting(key);
+
+        Console.WriteLine(value);
+    }
+}
---
+++ solution/Msh/Msh.UnitTests/PayCoinCorner/CoinCornerShould.cs
@@ -0,0 +1,59 @@
+﻿using Msh.Common.Models.Configuration;
+using Msh.Pay.CoinCorner.Models;
+using NUnit.Framework;
+using System.Text.Json;
+using Msh.TestSupport;
+
+namespace Msh.UnitTests.PayCoinCorner;
+
+[TestFixture]
+public class CoinCornerShould
+{
+    [Test]
+    [TestCase("Test Notes 1")]
+    [TestCase("Test Notes 2")]
+    public void AddConfigurationOnce(string notes)
+    {
+        var sut = TestConfigUtilities.GetRepository();
+
+        var coinCornerConfig = new CoinCornerConfig() { UserId = "USER"};
+
+        var json = JsonSerializer.Serialize(coinCornerConfig);
+
+        var config = new Config
+        {
+            ConfigType = "TestConfig",
+            Content = json,
+            Notes = notes
+
+        };
+       
+        Assert.That(true, Is.True, "Phase 1");
+
+        sut.AddConfig(config);
+        
+    }
+
+    [Test]
+    public void SaveExistingConfiguration()
+    {
+        var sut = TestConfigUtilities.GetRepository();
+
+        var config = sut.GetConfig("TestConfig");
+
+        var json = config.Content;
+
+        var ccConfig = JsonSerializer.Deserialize<CoinCornerConfig>(json);
+
+        Assert.That(ccConfig.UserId, Is.EqualTo("USER"));
+    }
+
+    [Test]
+    public void RemoveExistingConfiguration()
+    {
+        var sut = TestConfigUtilities.GetRepository();
+
+        sut.RemoveConfig("TestConfig");
+    }
+
+}
