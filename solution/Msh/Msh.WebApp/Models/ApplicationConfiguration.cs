62a22ea6d6b3e414eba02a9e71492a74c7d58e9f
Ron Murphy <ronmurp@gmail.com>
2024-08-13 15:20:31 +0100

    WebApp - Add ApplicationConfiguration

---
+++ solution/Msh/Msh.WebApp/Models/ApplicationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace Msh.WebApp.Models;
+
+/// <summary>
+/// Retrieves config settings - used in tests to get connection string
+/// </summary>
+public static class ApplicationConfiguration
+{
+    private static IConfigurationRoot _configuration;
+    static ApplicationConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        _configuration = builder.Build();
+    }
+    public static string GetSetting(string key)
+    {
+        return _configuration[key];
+    }
+}
+
