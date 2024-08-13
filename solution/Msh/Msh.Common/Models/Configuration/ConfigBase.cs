2c9c6445c2fb6e4bf324b8096a56dd7551e47bc8
Ron Murphy <ronmurp@gmail.com>
2024-08-13 15:06:52 +0100

    Common - Add ConfigBase

---
+++ solution/Msh/Msh.Common/Models/Configuration/ConfigBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Msh.Common.Models.Configuration;
+
+/// <summary>
+/// Abstract base class for config data that is to
+/// be stored in the Configs database table
+/// </summary>
+/// <param name="configType"></param>
+public abstract class ConfigBase(string configType)
+{
+    /// <summary>
+    /// Identify the Configs ConfigType name as the key for the type of data
+    /// </summary>
+    public string ConfigType { get; private set; } = configType;
+}
