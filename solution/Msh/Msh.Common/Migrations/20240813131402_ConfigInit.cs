8d43f65b7ce35dcbd319666622ab007bf154c0ba
Ron Murphy <ronmurp@gmail.com>
2024-08-13 15:17:14 +0100

    Common - Add Migrations

---
+++ solution/Msh/Msh.Common/Migrations/20240813131402_ConfigInit.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Msh.Common.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Configs",
+                columns: table => new
+                {
+                    ConfigType = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Configs", x => x.ConfigType);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Configs");
+        }
+    }
+}
