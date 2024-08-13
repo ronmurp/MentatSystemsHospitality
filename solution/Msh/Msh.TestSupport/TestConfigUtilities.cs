80ef4b1a147b9d0fe21d0965f3687f4cb85fa7d2
Ron Murphy <ronmurp@gmail.com>
2024-08-13 15:24:40 +0100

    TestSupport - Add project to support test projects

---
+++ solution/Msh/Msh.TestSupport/TestConfigUtilities.cs
@@ -0,0 +1,23 @@
+﻿using Msh.Common.Data;
+using Msh.WebApp.Models;
+
+namespace Msh.TestSupport;
+
+public static class TestConfigUtilities
+{
+    /// <summary>
+    /// Get and instance of the repo
+    /// </summary>
+    /// <returns></returns>
+    public static ConfigRepository GetRepository() =>
+        new(new ConfigDbContext(GetConnectionString()));
+
+
+    /// <summary>
+    /// Get the database connection string
+    /// </summary>
+    /// <returns></returns>
+    public static string GetConnectionString() =>
+        ApplicationConfiguration.GetSetting("ConnectionStrings:DefaultConnection");
+
+}
