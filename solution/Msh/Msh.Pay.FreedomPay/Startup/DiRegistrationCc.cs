06ad1edf3241e0ea7eadd3afbd926c8e37c14a7b
Ron Murphy <ronmurp@gmail.com>
2024-08-13 19:56:06 +0100

    FreedomPay - Add Di registration

---
+++ solution/Msh/Msh.Pay.FreedomPay/Startup/DiRegistrationCc.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Msh.Pay.FreedomPay.Services;
+
+namespace Msh.Pay.FreedomPay.Startup;
+
+public static class DiRegistrationCc
+{
+    public static void RegisterFpServices(this IServiceCollection services)
+    {
+        services.AddScoped<IFreedomPayCacheService, FreedomPayCacheService>();
+    }
+}
