a690b8d3f78825da181bfdb0e11ccd7293ec79ce
Ron Murphy <ronmurp@gmail.com>
2024-08-13 17:45:10 +0100

    Pay.FreedomPay - Add  FpErrorCode(Bank)

---
+++ solution/Msh/Msh.Pay.FreedomPay/Models/Configuration/FpErrorCode.cs
@@ -0,0 +1,23 @@
+﻿namespace Msh.Pay.FreedomPay.Models.Configuration;
+
+/// <summary>
+/// Error codes (ReasonCode). A list of these allows a ReasonCode to look up an associated message.
+/// </summary>
+public class FpErrorCode()
+{
+    public string Code { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Action level: 0=stay on page; 1=go to booking support, 2 = serious.
+    /// </summary>
+    public int Level { get; set; }
+
+    /// <summary>
+    /// If this is not empty, get the Message from the FpErrorCodeBank.Message
+    /// for the corresponding FpErrorCodeBank.
+    /// </summary>
+    public string Use { get; set; } = string.Empty;
+}
---
+++ solution/Msh/Msh.Pay.FreedomPay/Models/Configuration/FpErrorCodeBank.cs
@@ -0,0 +1,11 @@
+﻿namespace Msh.Pay.FreedomPay.Models.Configuration;
+
+/// <summary>
+/// A list of these forms a bank of messages, identified by Code,
+/// and used in FpErrorCode.Use
+/// </summary>
+public class FpErrorCodeBank()
+{
+    public string Code { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}
