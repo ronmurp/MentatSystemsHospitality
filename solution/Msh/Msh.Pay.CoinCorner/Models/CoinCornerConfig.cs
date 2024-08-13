3606300dc60eb12bd4d5a2e8237a3eb51fee7e80
Ron Murphy <ronmurp@gmail.com>
2024-08-13 17:33:39 +0100

    CoinCorner - Use ConstCc

--- solution/Msh/Msh.Pay.CoinCorner/Models/CoinCornerConfig.cs
+++ solution/Msh/Msh.Pay.CoinCorner/Models/CoinCornerConfig.cs
@@ -5,7 +5,7 @@ namespace Msh.Pay.CoinCorner.Models;
 /// <summary>
 /// All must be lower-case.
 /// </summary>
-public class CoinCornerConfig() : ConfigBase("CoinCornerConfig")
+public class CoinCornerConfig() : ConfigBase(ConstCc.CcConfig)
 {
     public string UserId { get; set; }
     public string PublicKey { get; set; }
