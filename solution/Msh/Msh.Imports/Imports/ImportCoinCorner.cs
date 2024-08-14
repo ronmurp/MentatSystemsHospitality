63a8e8a5269c7eea2ec16614c25f9cbcad85d942
Ron Murphy <ronmurp@gmail.com>
2024-08-14 20:12:08 +0100

    Refactor - Move SaveConfig to utilities

--- solution/Msh/Msh.Imports/Imports/ImportCoinCorner.cs
+++ solution/Msh/Msh.Imports/Imports/ImportCoinCorner.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using Msh.Common.Models.Configuration;
+using Msh.HotelCache.Models;
 using Msh.Pay.CoinCorner.Models;
 using Msh.TestSupport;
 using NUnit.Framework;
@@ -34,7 +35,7 @@ public class ImportCoinCorner
             LogFilePath = "Payments\\CoinCorner"
         };
 
-        SaveConfig("CoinCornerConfig", ccConfig);
+        TestConfigUtilities.SaveConfig(ConstCc.CoinCornerConfig, ccConfig);
     }
 
     /// <summary>
@@ -64,30 +65,7 @@ public class ImportCoinCorner
             StopWaitingPrompt = "Waiting for payment to complete ... or provisional payment."
         };
 
-        SaveConfig("CoinCornerGlobal", config);
-    }
-
-    private void SaveConfig<T>(string configType, T obj)
-    {
-        var now = DateTime.Now;
-
-        var json = JsonSerializer.Serialize(obj);
-
-        var sut = TestConfigUtilities.GetRepository();
-
-        var config = sut.GetConfig(configType);
-
-        if (config != null)
-        {
-            config.Content = json;
-            config.Notes = $"Update Import: {now:yyyy-MM-dd HH:mm:ss}";
-
-            sut.SaveConfig(config);
-            return;
-        }
-
-        sut.AddConfig(new Config { ConfigType = configType, Content = json, Notes = $"Initial Import: {now:yyyy-MM-dd HH:mm:ss}" });
-
+        TestConfigUtilities.SaveConfig(ConstCc.CoinCornerGlobal, config);
     }
 
 }
--- solution/Msh/Msh.TestSupport/TestConfigUtilities.cs
+++ solution/Msh/Msh.TestSupport/TestConfigUtilities.cs
@@ -1,5 +1,7 @@
 ﻿using Msh.Common.Data;
+using Msh.Common.Models.Configuration;
 using Msh.WebApp.Models;
+using System.Text.Json;
 
 namespace Msh.TestSupport;
 
@@ -19,5 +21,28 @@ public static class TestConfigUtilities
     /// <returns></returns>
     public static string GetConnectionString() =>
         ApplicationConfiguration.GetSetting("ConnectionStrings:DefaultConnection");
+
+    public static void SaveConfig<T>(string configType, T obj)
+    {
+        var now = DateTime.Now;
+
+        var json = JsonSerializer.Serialize(obj);
+
+        var sut = TestConfigUtilities.GetRepository();
+
+        var config = sut.GetConfig(configType);
+
+        if (config != null)
+        {
+            config.Content = json;
+            config.Notes = $"Update Import: {now:yyyy-MM-dd HH:mm:ss}";
+
+            sut.SaveConfig(config);
+            return;
+        }
+
+        sut.AddConfig(new Config { ConfigType = configType, Content = json, Notes = $"Initial Import: {now:yyyy-MM-dd HH:mm:ss}" });
+
+    }
 
 }
