96c1647d38c96bb474faedb9fd60dae0d292a4aa
Ron Murphy <ronmurp@gmail.com>
2024-08-14 18:06:21 +0100

    UnitTests - Add date time

---
+++ solution/Msh/Msh.UnitTests/Basic/DateTimeShould.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Msh.UnitTests.Basic;
+
+[TestFixture]
+public class DateTimeShould
+{
+    [Test]
+    public void ShowUtc()
+    {
+        var dtu = DateTime.UtcNow;
+        var dto = DateTimeOffset.UtcNow;
+        var dt = DateTime.Now;
+        
+
+        Console.WriteLine($"{dto:O} - {dto.ToLocalTime():O}");
+        Console.WriteLine($"{dt:O} - {dt.ToLocalTime():O}");
+        Console.WriteLine($"{dtu:O} - {dtu.ToLocalTime():O}");
+    }
+}
