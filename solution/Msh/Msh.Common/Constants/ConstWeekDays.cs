9efe448735f50be6ae98baded14994561d322cc2
Ron Murphy <ronmurp@gmail.com>
2024-08-14 19:19:10 +0100

    Common - Add Weekday constants

---
+++ solution/Msh/Msh.Common/Constants/ConstWeekDays.cs
@@ -0,0 +1,12 @@
+﻿namespace Msh.Common.Constants;
+
+public static class WeekDays
+{
+    public const string Mon = "Mon";
+    public const string Tue = "Tue";
+    public const string Wed = "Wed";
+    public const string Thu = "Thu";
+    public const string Fri = "Fri";
+    public const string Sat = "Sat";
+    public const string Sun = "Sun";
+}
