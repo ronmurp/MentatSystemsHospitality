2cb58d1b9f5fbd90e3800b97afad3c19c604e63f
Ron Murphy <ronmurp@gmail.com>
2024-08-16 00:02:03 +0100

    WebApp - Update controller for CC edit

--- solution/Msh/Msh.WebApp/Controllers/Admin/CoinCorner/CoinCornerController.cs
+++ solution/Msh/Msh.WebApp/Controllers/Admin/CoinCorner/CoinCornerController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Msh.Pay.CoinCorner.Models;
+using Msh.Pay.CoinCorner.Services;
 
 namespace Msh.WebApp.Controllers.Admin.CoinCorner;
 
@@ -6,21 +8,46 @@ namespace Msh.WebApp.Controllers.Admin.CoinCorner;
 public class CoinCornerController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ICoinCornerCacheService _coinCornerCacheService;
+    private readonly ICoinCornerRepoService _coinCornerRepoService;
 
-    public CoinCornerController(ILogger<HomeController> logger)
+    public CoinCornerController(ILogger<HomeController> logger, 
+	    ICoinCornerCacheService coinCornerCacheService,
+	    ICoinCornerRepoService coinCornerRepoService)
     {
-        _logger = logger;
+	    _logger = logger;
+	    _coinCornerCacheService = coinCornerCacheService;
+	    _coinCornerRepoService = coinCornerRepoService;
     }
 
     [Route("")]
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+	    await Task.Delay(0);
+
         return View("~/Views/Admin/CoinCorner/Index.cshtml");
     }
 
     [Route("Config")]
-    public IActionResult Xxx()
+    public async Task<IActionResult> Config()
     {
-        return View("~/Views/Admin/CoinCorner/Config.cshtml");
+	    await Task.Delay(0);
+
+	    var config = _coinCornerRepoService.GetConfig();
+
+		return View("~/Views/Admin/CoinCorner/Config.cshtml", config);
+    }
+
+    [HttpPost]
+    [Route("ConfigEdit")]
+    public async Task<IActionResult> ConfigEdit([FromForm] CoinCornerConfig config)
+    {
+	    await Task.Delay(0);
+
+	    _coinCornerRepoService.SaveConfig(config);
+
+
+		return RedirectToAction("Config");
+	    // return View("~/Views/Admin/CoinCorner/Config.cshtml", config);
     }
 }
