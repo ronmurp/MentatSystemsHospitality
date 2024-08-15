69dae7b97ed05496e695ec64264f3f22aa0dbddf
Ron Murphy <ronmurp@gmail.com>
2024-08-16 00:03:18 +0100

    WebApp - Add tag helper

---
+++ solution/Msh/Msh.WebApp/TagHelpers/IconInfoTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Msh.WebApp.TagHelpers;
+
+[HtmlTargetElement("info-icon")]
+public class IconInfoTagHelper(IHtmlGenerator generator) : AnchorTagHelper(generator)
+{
+	[HtmlAttributeName("name")]
+	public string IconInfoName { get; set; } = string.Empty;
+
+	public override void Process(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "a";
+		output.TagMode = TagMode.StartTagAndEndTag;
+		output.Attributes.Add("href", $"javascript:mshMethods.showInfo('{IconInfoName}')");
+		output.Content.SetHtmlContent("<i class=\"fa fa-info-circle\"></i>");
+		
+		base.Process(context, output);
+	}
+}
