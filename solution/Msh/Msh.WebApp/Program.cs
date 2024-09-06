6a13f341cbfa37c7b8b9fdc50348c3c64ca02d50
Ron Murphy <ronmurp@gmail.com>
2024-09-06 17:41:58 +0100

    WebApp - Add runtime compilation for razor

--- solution/Msh/Msh.WebApp/Program.cs
+++ solution/Msh/Msh.WebApp/Program.cs
@@ -26,6 +26,9 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 
 builder.Services.AddControllersWithViews();
 
+// Make this conditional - with a pragma, or with a configuration option - needs reload of system
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 // Register our services
 builder.LoadServices();
 
