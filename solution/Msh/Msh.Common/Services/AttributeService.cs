2e6813328c61d8e11a0d83edc570ab61af5fe6db
Ron Murphy <ronmurp@gmail.com>
2024-09-06 14:39:49 +0100

    Common - Add PropertyValueService and AttributeService...
    To get attribute values to be used in client side editing.

---
+++ solution/Msh/Msh.Common/Services/AttributeService.cs
@@ -0,0 +1,72 @@
+﻿using System.Reflection;
+
+namespace Msh.Common.Services;
+
+/// <summary>
+/// Get attributes from PropertyInfo
+/// </summary>
+public class AttributeService
+{
+	public string GetDescriptionAttribute(PropertyInfo prop)
+	{
+		var attributes = prop.GetCustomAttributes(true);
+		var attr = GetAttribute(attributes, "DescriptionAttribute");
+		if (attr != null)
+		{
+			var y = (System.ComponentModel.DescriptionAttribute)attr;
+			return y.Description;
+		}
+		return string.Empty;
+	}
+
+	public string GetCategoryAttribute(PropertyInfo prop)
+	{
+		var attributes = prop.GetCustomAttributes(true);
+		var attr = GetAttribute(attributes, "CategoryAttribute");
+		if (attr != null)
+		{
+			var y = (System.ComponentModel.CategoryAttribute)attr;
+			return y.Category;
+		}
+		return string.Empty;
+	}
+
+	public string GetInfoAttribute(PropertyInfo prop)
+	{
+		var attributes = prop.GetCustomAttributes(true);
+		var attr = GetAttribute(attributes, "InfoAttribute");
+		if (attr != null)
+		{
+			var y = (Msh.Common.Attributes.InfoAttribute)attr;
+			return y.Info;
+
+		}
+		return string.Empty;
+	}
+
+	public string GetCssClassAttribute(PropertyInfo prop)
+	{
+		var attributes = prop.GetCustomAttributes(true);
+		var attr = GetAttribute(attributes, "CssClassAttribute");
+		if (attr != null)
+		{
+			var y = (Msh.Common.Attributes.CssClassAttribute)attr;
+			return y.CssClass;
+
+		}
+		return string.Empty;
+	}
+
+	private object? GetAttribute(object[] att, string name)
+	{
+		if (att.Length > 0)
+		{
+			var s = att.FirstOrDefault(a => a.GetType().Name == name);
+			if (s != null)
+			{
+				return s;
+			}
+		}
+		return null;
+	}
+}
---
+++ solution/Msh/Msh.Common/Services/PropertyValueService.cs
@@ -0,0 +1,54 @@
+﻿using Msh.Common.Attributes;
+
+namespace Msh.Common.Services;
+
+public class PropertyValueService : AttributeService
+{
+	public List<PropertyValues> GetProperties(Type classType)
+	{
+		var list = new List<PropertyValues>();
+
+		var properties = classType.GetProperties();
+		foreach (var prop in properties)
+		{
+			var pv = new PropertyValues
+			{
+				Name = prop.Name,
+				DataType = $"{prop.PropertyType}",
+				Category = GetCategoryAttribute(prop)
+			};
+
+
+			switch (pv.DataType)
+			{
+				case "System.String" when pv.Category == "Html":
+					break;
+
+				case "System.String" when pv.Category == "TextArea":
+					break;
+
+				case "System.String":
+					break;
+
+				case "System.Decimal":
+					break;
+
+				case "System.Boolean":
+					break;
+
+				case "System.Int32":
+					break;
+
+				case "System.DateTime":
+					break;
+
+				default:
+					break;
+			}
+
+			list.Add(pv);
+		}
+
+		return list;
+	}
+}
