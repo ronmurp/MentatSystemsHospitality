d1f4c33ea0feb8640d21f3da752894835e8a0581
Ron Murphy <ronmurp@gmail.com>
2024-09-06 14:56:19 +0100

    Add an error block data for use in views
    Under evaluation

---
+++ solution/Msh/Msh.WebApp/Models/Admin/ErrorBlock.cs
@@ -0,0 +1,10 @@
+﻿namespace Msh.WebApp.Models.Admin;
+
+/// <summary>
+/// How an error message is passed to an error view
+/// </summary>
+public class ErrorBlock
+{
+	public string Message { get; set; } = string.Empty;
+	public string CssClass { get; set; } = "error-block";
+}
