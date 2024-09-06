350a106b70d716485c52065c2ec6c333008bcfa3
Ron Murphy <ronmurp@gmail.com>
2024-09-06 14:33:36 +0100

    Common - Add PropertyValues to hold property properties

---
+++ solution/Msh/Msh.Common/Attributes/PropertyValues.cs
@@ -0,0 +1,27 @@
+﻿namespace Msh.Common.Attributes;
+
+/// <summary>
+/// A class that describes information related to a property on a class.
+/// For use in javascript.
+/// </summary>
+public class PropertyValues
+{
+	/// <summary>
+	/// Name of the property
+	/// </summary>
+	public string Name { get; set; } = string.Empty;
+
+	/// <summary>
+	/// String representation of the property's type, example: System.Boolean 
+	/// </summary>
+	public string DataType { get; set; } = string.Empty;
+
+	/// <summary>
+	/// An additional category that determines the type of editor to use
+	/// </summary>
+	/// <remarks>
+	/// System.String - Text input (default), TextArea, Html (e.g., Monaco editor),
+	/// See ConstCom.PropCat 
+	/// </remarks>
+	public string Category { get; set; } = string.Empty;
+}
--- solution/Msh/Msh.Common/Constants/ConstCom.cs
+++ solution/Msh/Msh.Common/Constants/ConstCom.cs
@@ -1,6 +1,16 @@
 ﻿namespace Msh.Common.Constants;
 
-public partial class ConstCom
+public static partial class ConstCom
 {
+	/// <summary>
+	/// Property categories - see PropertyValues class.
+	/// The Category attribute on classes
+	/// </summary>
+	public static class PropCat
+	{
+		public const string TextArea = "TextArea";
+		public const string Html = "Html";
+
+	}
 
 }
