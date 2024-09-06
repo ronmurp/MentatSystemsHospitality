a28b66e25c33709b767ae3cd2981a9aca4706413
Ron Murphy <ronmurp@gmail.com>
2024-09-06 14:36:31 +0100

    Common - Add view models for API

---
+++ solution/Msh/Msh.Common/Models/ViewModels/BaseVm.cs
@@ -0,0 +1,31 @@
+﻿namespace Msh.Common.Models.ViewModels;
+
+public class BaseVm
+{
+	public BaseVm()
+	{
+
+	}
+
+	public BaseVm(string errorMessage)
+	{
+		Success = false;
+		ErrorMessage = errorMessage;
+	}
+	public bool Success { get; set; } = true;
+	public string ErrorMessage { get; set; } = string.Empty;
+
+	/// <summary>
+	/// A message (may be html) that can be returned on success.
+	/// </summary>
+	public string Message { get; set; } = string.Empty;
+	/// <summary>
+	/// A message (may be html) that can be returned on success.
+	/// </summary>
+	public string UserErrorMessage { get; set; } = string.Empty;
+	public bool UserMessageIsHtml { get; set; }
+	/// <summary>
+	/// Any data that might need to be added
+	/// </summary>
+	public object? Data { get; set; }
+}
---
+++ solution/Msh/Msh.Common/Models/ViewModels/ObjectVm.cs
@@ -0,0 +1,10 @@
+﻿namespace Msh.Common.Models.ViewModels;
+
+/// <summary>
+/// A simple model for passing data to views via json responses.
+/// Script will determine the model content based on context.
+/// </summary>
+public class ObjectVm : BaseVm
+{
+	
+}
