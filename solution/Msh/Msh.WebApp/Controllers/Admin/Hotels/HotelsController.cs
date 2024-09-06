2b99deca11c48e3af18630a0c8e1a00a4684bf2c
Ron Murphy <ronmurp@gmail.com>
2024-09-06 17:40:55 +0100

    WebApp - Remove API (now in own file)

--- solution/Msh/Msh.WebApp/Controllers/Admin/Hotels/HotelsController.cs
+++ solution/Msh/Msh.WebApp/Controllers/Admin/Hotels/HotelsController.cs
@@ -2,7 +2,6 @@
 using Msh.Common.Data;
 using Msh.Common.Exceptions;
 using Msh.Common.Models.ViewModels;
-using Msh.Common.Services;
 using Msh.HotelCache.Models;
 using Msh.HotelCache.Models.Hotels;
 using Msh.HotelCache.Models.RoomTypes;
@@ -286,95 +285,5 @@ namespace Msh.WebApp.Controllers.Admin.Hotels
 
 		
 		
-	}
-
-	[ApiController]
-	[Route("api/hotelapi")]
-	public class HotelApiController : Controller
-	{
-		[HttpGet]
-		[Route("hotelSave")]
-		public async Task<IActionResult> HotelSave()
-		{
-			try
-			{
-				await Task.Delay(0);
-
-				return Ok(new ObjectVm
-				{
-					Data = new Hotel()
-				});
-			}
-			catch (Exception ex)
-			{
-				return Ok(new ObjectVm
-				{
-					Success = false,
-					UserErrorMessage = ex.Message
-				});
-			}
-		}
-
-		[HttpPost]
-		[Route("hotelSave")]
-		public async Task<IActionResult> HotelSave(Hotel hotel)
-		{
-			try
-			{
-				await Task.Delay(0);
-
-				return Ok(new ObjectVm
-				{
-					Data = hotel
-				});
-			}
-			catch (Exception ex)
-			{
-				return Ok(new ObjectVm
-				{
-					Success = false,
-					UserErrorMessage = ex.Message
-				});
-			}
-		}
-
-		
-		[HttpGet]
-		[Route("hotelConfig")]
-		public async Task<IActionResult> HotelConfig()
-		{
-			return await GetConfig(typeof(Hotel));
-		}
-
-		[HttpGet]
-		[Route("roomTypeConfig")]
-		public async Task<IActionResult> RoomTypeConfig()
-		{
-			return await GetConfig(typeof(RoomType));
-		}
-
-		private async Task<IActionResult> GetConfig(Type classType)
-		{
-			try
-			{
-				await Task.Delay(0);
-
-				var propService = new PropertyValueService();
-				var list = propService.GetProperties(classType);
-				return Ok(new ObjectVm
-				{
-					Data = list
-				});
-			}
-			catch (Exception ex)
-			{
-				return Ok(new ObjectVm
-				{
-
-					UserErrorMessage = ex.Message
-				});
-			}
-		}
-
 	}
 }
