b9218df4c70da8673c4f7929b942477984d42102
Ron Murphy <ronmurp@gmail.com>
2024-09-06 15:55:07 +0100

    WebApp - Add/Update Hotel admin controller
    Add api controller - experimental

--- solution/Msh/Msh.WebApp/Controllers/Admin/Hotels/HotelsController.cs
+++ solution/Msh/Msh.WebApp/Controllers/Admin/Hotels/HotelsController.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Msh.Common.Data;
 using Msh.Common.Exceptions;
+using Msh.Common.Models.ViewModels;
+using Msh.Common.Services;
 using Msh.HotelCache.Models;
 using Msh.HotelCache.Models.Hotels;
+using Msh.HotelCache.Models.RoomTypes;
 using Msh.HotelCache.Services;
+using Msh.WebApp.Models.Admin.ViewModels;
+using System.Collections.Generic;
 
 namespace Msh.WebApp.Controllers.Admin.Hotels
 {
@@ -79,6 +84,25 @@ namespace Msh.WebApp.Controllers.Admin.Hotels
 			return RedirectToAction("HotelList");
 		}
 
+		[Route("HotelEdit/{hotelCode}")]
+		public async Task<IActionResult> HotelEdit(string hotelCode)
+		{
+			try
+			{
+				await Task.Delay(0);
+
+				var hotels = hotelsRepoService.GetHotels();
+				var hotel = hotels.FirstOrDefault(h => h.HotelCode == hotelCode);
+				return View("~/Views/Admin/Hotels/HotelEdit.cshtml", hotel);
+			}
+			catch (Exception ex)
+			{
+				logger.LogError($"{ex.Message}");
+			}
+
+			return RedirectToAction("HotelList");
+		}
+
 		[Route("HotelEditByCode")]
 		public async Task<IActionResult> HotelEditByCode([FromQuery]string hotelCode, [FromQuery]string action)
 		{
@@ -105,7 +129,7 @@ namespace Msh.WebApp.Controllers.Admin.Hotels
 				{
 					var hotel = hotels.FirstOrDefault(h => h.HotelCode == hotelCode);
 
-					return RedirectToAction("HotelEdit", hotel);
+					return Redirect($"HotelEdit/{hotelCode}");
 				}
 				
 
@@ -163,5 +187,194 @@ namespace Msh.WebApp.Controllers.Admin.Hotels
 
 			return RedirectToAction("HotelList");
 		}
+
+
+		[HttpPost]
+		[Route("HotelSave2")]
+		public async Task<IActionResult> HotelSave2(Hotel hotel)
+		{
+			try
+			{
+				await Task.Delay(0);
+
+				if (!ModelState.IsValid)
+				{
+					return Ok(new ObjectVm
+					{
+						Success = false
+
+					});
+
+				}
+
+				var hotelList = hotelsRepoService.GetHotels();
+				var found = false;
+				for (var i = 0; i < hotelList.Count; i++)
+				{
+					if (hotelList[i].HotelCode == hotel.HotelCode)
+					{
+						hotelList[i] = hotel;
+						found = true;
+					}
+				}
+
+				if (!found)
+				{
+					hotelList.Add(hotel);
+					found = true;
+				}
+
+				if (found)
+				{
+					hotelsRepoService.SaveHotels(hotelList);
+				}
+
+
+				return RedirectToAction("HotelList");
+			}
+			catch (Exception ex)
+			{
+				logger.LogError($"{ex.Message}");
+			}
+
+			return RedirectToAction("HotelList");
+		}
+
+		[Route("RoomTypeList")]
+		public async Task<IActionResult> RoomTypeList([FromQuery] string hotelCode)
+		{
+			var vm = new RoomTypeListVm
+			{
+				HotelCode = string.IsNullOrEmpty(hotelCode) ? string.Empty : hotelCode
+			};
+
+			try
+			{
+				await Task.Delay(0);
+
+				vm.Hotels = hotelsRepoService.GetHotels();
+				var hotel = string.IsNullOrEmpty(hotelCode)
+					? vm.Hotels.FirstOrDefault()
+					: vm.Hotels.FirstOrDefault(h => h.HotelCode == hotelCode);
+
+				vm.HotelCode = hotel != null ? hotel.HotelCode : string.Empty;
+
+				var roomTypes = hotelsRepoService.GetRoomTypes(vm.HotelCode);
+
+				vm.RoomTypes = roomTypes;
+
+				return View("~/Views/Admin/Hotels/RoomTypeList.cshtml", vm);
+			}
+			catch (NullConfigException ex)
+			{
+				if (!string.IsNullOrEmpty(vm.HotelCode))
+				{
+					configRepository.SaveMissingConfig(ConstHotel.Cache.RoomTypes, vm.HotelCode, new List<RoomType>());
+				}
+
+				vm.ErrorMessage = $"No room types for hotel {vm.HotelCode}";
+				
+				return View("~/Views/Admin/Hotels/RoomTypeList.cshtml", vm);
+			}
+			catch (Exception ex)
+			{
+				logger.LogError($"{ex.Message}");
+				vm.ErrorMessage = $"Error for hotel {vm.HotelCode}. {ex.Message}";
+				return View("~/Views/Admin/Hotels/RoomTypeList.cshtml", vm);
+			}
+		}
+
+		
+		
+	}
+
+	[ApiController]
+	[Route("api/hotelapi")]
+	public class HotelApiController : Controller
+	{
+		[HttpGet]
+		[Route("hotelSave")]
+		public async Task<IActionResult> HotelSave()
+		{
+			try
+			{
+				await Task.Delay(0);
+
+				return Ok(new ObjectVm
+				{
+					Data = new Hotel()
+				});
+			}
+			catch (Exception ex)
+			{
+				return Ok(new ObjectVm
+				{
+					Success = false,
+					UserErrorMessage = ex.Message
+				});
+			}
+		}
+
+		[HttpPost]
+		[Route("hotelSave")]
+		public async Task<IActionResult> HotelSave(Hotel hotel)
+		{
+			try
+			{
+				await Task.Delay(0);
+
+				return Ok(new ObjectVm
+				{
+					Data = hotel
+				});
+			}
+			catch (Exception ex)
+			{
+				return Ok(new ObjectVm
+				{
+					Success = false,
+					UserErrorMessage = ex.Message
+				});
+			}
+		}
+
+		
+		[HttpGet]
+		[Route("hotelConfig")]
+		public async Task<IActionResult> HotelConfig()
+		{
+			return await GetConfig(typeof(Hotel));
+		}
+
+		[HttpGet]
+		[Route("roomTypeConfig")]
+		public async Task<IActionResult> RoomTypeConfig()
+		{
+			return await GetConfig(typeof(RoomType));
+		}
+
+		private async Task<IActionResult> GetConfig(Type classType)
+		{
+			try
+			{
+				await Task.Delay(0);
+
+				var propService = new PropertyValueService();
+				var list = propService.GetProperties(classType);
+				return Ok(new ObjectVm
+				{
+					Data = list
+				});
+			}
+			catch (Exception ex)
+			{
+				return Ok(new ObjectVm
+				{
+
+					UserErrorMessage = ex.Message
+				});
+			}
+		}
+
 	}
 }
