ea8a62ebd67a559023d468bbc80e4e4541a4d26d
Ron Murphy <ronmurp@gmail.com>
2024-09-07 01:45:09 +0100

    WebApp - Add success alert shared view and model

---
+++ solution/Msh/Msh.WebApp/Models/Admin/SuccessAlert.cs
@@ -0,0 +1,14 @@
+﻿namespace Msh.WebApp.Models.Admin;
+
+/// <summary>
+/// Parameters to pass to _AdminSaveSuccess
+/// </summary>
+public class SuccessAlert
+{
+
+	public string ModelName { get; set; } = string.Empty;
+	public string ReturnPath { get; set; } = string.Empty;
+
+	public string ReturnPathName { get; set; } = string.Empty;
+
+}
