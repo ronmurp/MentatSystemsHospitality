7bfc5874b9fb05fc7bc1cd3fc44f9521c522cf0c
Ron Murphy <ronmurp@gmail.com>
2024-09-09 22:51:34 +0100

    Update with comments

--- solution/Msh/Msh.WebApp/Components/AdminHotelSelectViewComponent.cs
+++ solution/Msh/Msh.WebApp/Components/AdminHotelSelectViewComponent.cs
@@ -4,8 +4,17 @@ using Msh.WebApp.Models.Admin.ViewModels;
 
 namespace Msh.WebApp.Components;
 
+/// <summary>
+/// Presents an *Admin* hotel select based on database config data
+/// </summary>
+/// <param name="hotelsRepoService"></param>
 public class AdminHotelSelectViewComponent(IHotelsRepoService hotelsRepoService) : ViewComponent
 {
+	/// <summary>
+	/// Invoke the view components with the selected hotel code
+	/// </summary>
+	/// <param name="hotelCode"></param>
+	/// <returns></returns>
 	public async Task<IViewComponentResult> InvokeAsync(string hotelCode)
 	{
 		var hotels = await hotelsRepoService.GetHotelsAsync();
@@ -18,5 +27,4 @@ public class AdminHotelSelectViewComponent(IHotelsRepoService hotelsRepoService)
 
 		return View(vm);
 	}
-
 }
