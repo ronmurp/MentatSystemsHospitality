ab05a6369fe63e9319f956887fb0917cc530f34f
Ron Murphy <ronmurp@gmail.com>
2024-09-09 18:21:39 +0100

    Change admin hotel select to a view component

---
+++ solution/Msh/Msh.WebApp/Components/AdminHotelSelectViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Msh.HotelCache.Services;
+using Msh.WebApp.Models.Admin.ViewModels;
+
+namespace Msh.WebApp.Components;
+
+public class AdminHotelSelectViewComponent(IHotelsRepoService hotelsRepoService) : ViewComponent
+{
+	public async Task<IViewComponentResult> InvokeAsync(string hotelCode)
+	{
+		var hotels = await hotelsRepoService.GetHotelsAsync();
+
+		var vm = new HotelListVm
+		{
+			HotelCode = hotelCode,
+			Hotels = hotels
+		};
+
+		return View(vm);
+	}
+
+}
