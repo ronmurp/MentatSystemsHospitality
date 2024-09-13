38423fefdf35f489ffba5b0a58c0e86175270888
Ron Murphy <ronmurp@gmail.com>
2024-09-13 14:44:01 +0100

    REFACTOR - Update extension methods

--- solution/Msh/Msh.HotelCache/ExtensionMethods/HotelExtensionMethods.cs
+++ solution/Msh/Msh.HotelCache/ExtensionMethods/HotelExtensionMethods.cs
@@ -7,19 +7,19 @@ namespace Msh.HotelCache.ExtensionMethods;
 public static class HotelExtensionMethods
 {
     public static bool HasDates(this Hotel hotel) => 
-        hotel.HotelDateList.Count > 0;
+        hotel.StayDates.Count > 0;
 
     public static bool IsDisabled(this Hotel hotel, DateOnly arrive, DateOnly depart) => 
-        hotel.HotelDateList.Any(hdi => hdi.IsDisabled(arrive, depart)) 
+        hotel.StayDates.Any(hdi => hdi.IsDisabled(arrive, depart)) 
         || !string.IsNullOrEmpty(hotel.DisabledText);
 
-    public static HotelDateItem? InvalidDate(this Hotel hotel, DateOnly arrive, DateOnly depart) => 
-        hotel.HotelDateList.FirstOrDefault(d => d.IsDisabled(arrive, depart));
+    public static ItemDate? InvalidDate(this Hotel hotel, DateOnly arrive, DateOnly depart) => 
+        hotel.StayDates.FirstOrDefault(d => d.IsDisabled(arrive, depart));
 
     public static bool CannotBook(this Hotel hotel, DateOnly now) => 
-        hotel.HotelDateList.Exists(d => d.CannotBook(now));
+        hotel.StayDates.Exists(d => d.CannotBook(now));
 
-    public static HotelDateItem? CannotBookDate(this Hotel hotel, DateOnly now) => 
-        hotel.HotelDateList.FirstOrDefault(d => d.CannotBook(now));
+    public static ItemDate? CannotBookDate(this Hotel hotel, DateOnly now) => 
+        hotel.StayDates.FirstOrDefault(d => d.CannotBook(now));
 
 }
---
+++ solution/Msh/Msh.HotelCache/ExtensionMethods/RatePlanExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿using Msh.Common.Constants;
+using Msh.Common.ExtensionMethods;
+using Msh.HotelCache.Models.RatePlans;
+
+namespace Msh.HotelCache.ExtensionMethods;
+
+public static class RatePlanExtensionMethods
+{
+	public static string StayFromString(this RoomRatePlan ratePlan) => ratePlan.StayFrom.ToString(DateConst.DateFormat);
+	public static string StayToString(this RoomRatePlan ratePlan) => ratePlan.StayTo.ToString(DateConst.DateFormat);
+
+	public static (bool valid, string fromSpan, string toSpan, string comment)
+		ValidBookDates(this RoomRatePlan rp, DateOnly today) =>
+		!rp.HasBookDates
+			? (true, DateConst.SpanInActive, DateConst.SpanInActive, string.Empty) // Ignore if not using book dates
+
+			: rp.BookFrom.IsMin() && rp.BookTo.IsMax()
+				? (true, DateConst.SpanActive, DateConst.SpanActive, string.Empty)  // Ignore if Min-Max - Book any time
+
+				: rp.BookFrom >= rp.BookTo
+					? (false, DateConst.SpanError, DateConst.SpanError, DateConst.FromAfterTo)
+
+					: rp.BookFrom >= rp.StayTo
+						? (false, DateConst.SpanError, DateConst.SpanError, DateConst.BookTooLate)
+
+						: rp.BookTo >= rp.StayTo
+							? (true, DateConst.SpanNormal, DateConst.SpanError, DateConst.BookToIsLate)
+
+							: rp.BookTo < today
+								? (true, DateConst.SpanNormal, DateConst.SpanError, DateConst.BookToPast)
+
+								: (true, DateConst.SpanNormal, DateConst.SpanNormal, string.Empty);
+}
