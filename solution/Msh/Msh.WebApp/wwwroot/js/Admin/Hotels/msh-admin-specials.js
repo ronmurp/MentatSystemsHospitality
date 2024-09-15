6acf4ff95f9f6bbc648adec393066bcd042f776d
Ron Murphy <ronmurp@gmail.com>
2024-09-15 23:18:46 +0100

    WebApp - Add CSS and javascript for specials

---
+++ solution/Msh/Msh.WebApp/wwwroot/js/Admin/Hotels/msh-admin-specials.js
@@ -0,0 +1,46 @@
+﻿(function ($) {
+
+    "use strict";
+    var app = mshPageApp;
+    var meth = app.methodsService;
+    var util = app.utilityService;
+    var modal = app.modalService;
+    var api = app.apiService;
+
+    var itemDatesService = app.itemDatesService;
+
+    var ids = {
+        selectHotel: '#selectHotel'
+    }
+
+    function getHotelCode() {
+        var hotelCode = $(ids.selectHotel).val();
+        hotelCode = hotelCode ? hotelCode : $('#hotelCode').val();
+        return hotelCode;
+    }
+
+    $(ids.selectHotel).on('change', () => {
+        var hotelCode = getHotelCode();
+        util.redirectTo(`admin/hotels/SpecialsList?hotelCode=${hotelCode}`);
+    });
+
+    if (app.itemDatesService) {
+        app.itemDatesService.init({ datesApiAction: 'SpecialsDates' });
+    }
+   
+    app.hotelActionService.init({
+        deleteApi: '/api/hotelapi/SpecialDelete',
+        copyApi: '/api/hotelapi/SpecialCopy',
+        listPath: 'admin/hotels/SpecialsList'
+    });
+
+    app.hotelActionBulkService.init({
+        deleteBulkApi: '/api/hotelapi/SpecialDeleteBulk',
+        copyBulkApi: '/api/hotelapi/SpecialCopyBulk',
+        sortListApi: '/api/hotelapi/SpecialsSort',
+        listPath: 'admin/hotels/SpecialsList'
+    });
+
+
+
+}(jQuery));
