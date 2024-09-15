630fe2dd04d905d0829ea1b820d8d3ad504a8fed
Ron Murphy <ronmurp@gmail.com>
2024-09-15 23:16:33 +0100

    WebApp - Add SpecialsListVm

---
+++ solution/Msh/Msh.WebApp/Models/Admin/ViewModels/SpecialsListVm.cs
@@ -0,0 +1,8 @@
+﻿using Msh.HotelCache.Models.Specials;
+
+namespace Msh.WebApp.Models.Admin.ViewModels;
+
+public class SpecialsListVm : HotelListVm
+{
+	public List<Special> Specials { get; set; } = [];
+}
