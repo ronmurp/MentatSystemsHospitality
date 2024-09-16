28507d434d88162a3e9f9dc5449fb6462dbb3b8b
Ron Murphy <ronmurp@gmail.com>
2024-09-16 16:49:53 +0100

    Add model and markup for common table cells: up/down/edit/bolk/copy

---
+++ solution/Msh/Msh.WebApp/Models/Admin/ViewModels/TableCommonCellsVm.cs
@@ -0,0 +1,12 @@
+﻿namespace Msh.WebApp.Models.Admin.ViewModels;
+
+public class TableCommonCellsVm
+{
+	public string HotelCode { get; set; } = string.Empty;
+	public string Code { get; set; } = string.Empty;
+
+	public string EditAction { get; set; } = string.Empty;
+	public int Index { get; set; }
+
+	public int Count { get; set; }
+}
