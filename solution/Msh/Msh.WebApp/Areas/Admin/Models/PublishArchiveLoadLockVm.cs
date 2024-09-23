454bfa44f666e9f55b527b170c22cdd0b2a068d2
Ron Murphy <ronmurp@gmail.com>
2024-09-23 19:14:42 +0100

    WebApp - Add partial view for Publish, Archive, Load Lock buttons

---
+++ solution/Msh/Msh.WebApp/Areas/Admin/Models/PublishArchiveLoadLockVm.cs
@@ -0,0 +1,15 @@
+﻿namespace Msh.WebApp.Areas.Admin.Models;
+
+public class PublishArchiveLoadLockVm
+{
+	public string OnclickPublish { get; set; } = "window.mshMethods.publishHotels()";
+	public string OnclickArchive { get; set; } = "window.mshMethods.archiveHotels()";
+
+	public string OnclickLoad { get; set; } = "window.mshMethods.loadHotels()";
+
+	public string OnclickLock { get; set; } = "window.mshMethods.lockHotels()";
+
+	public string Controller { get; set; } = "Hotels";
+
+	public string Action { get; set; } = "Index";
+}
