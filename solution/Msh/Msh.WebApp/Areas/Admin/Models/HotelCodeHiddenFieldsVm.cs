51986a31263e4f2a189d998494e9eda80c203ea2
Ron Murphy <ronmurp@gmail.com>
2024-09-26 12:38:51 +0100

    REAFCTOR Add hidden code fields

---
+++ solution/Msh/Msh.WebApp/Areas/Admin/Models/HotelCodeHiddenFieldsVm.cs
@@ -0,0 +1,13 @@
+﻿namespace Msh.WebApp.Areas.Admin.Models;
+
+/// <summary>
+/// View model for _HotelCodeHiddenFields.cshtml
+/// </summary>
+public class HotelCodeHiddenFieldsVm
+{
+	public bool UseHotel { get; set; }
+	public string? HotelCode { get; set; } = string.Empty;
+
+	public string? Code { get; set; } = string.Empty;
+	public string? EditType { get; set; }
+}
