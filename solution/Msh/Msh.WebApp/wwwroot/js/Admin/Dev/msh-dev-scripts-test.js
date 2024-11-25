8d6a772c68d82a84a277d6e422b574a3dcf58ba6
Ron Murphy <ronmurp@gmail.com>
2024-11-25 15:31:03 +0000

    Update script tests to include overlays

--- solution/Msh/Msh.WebApp/wwwroot/js/Admin/Dev/msh-dev-scripts-test.js
+++ solution/Msh/Msh.WebApp/wwwroot/js/Admin/Dev/msh-dev-scripts-test.js
@@ -1,7 +1,49 @@
 ﻿(function ($) {
 
     var app = window.mshPageApp;
+    var meth = app.methodsService;
     var modal = app.modalService;
+    var mas = app.modalActionService;
+
+    var ids = {
+        selectHotel: '#selectHotel'
+    }
+
+    var apiRoot = '/api/hotelapi';
+
+    var options = {
+        modalActionId: 'confirm-action',
+        modalActionTitle: 'Confirm Action',
+        modalActionBody: 'Confirm that an action should be carried out.',
+        modalActionOnCLick: `id="confirm-load-ok" onclick="window.mshMethods.testConfirmed()"`,
+        modalActionOk: 'OK',
+        loadConfirmApiUrl: `${apiRoot}/RatePlanSortLoad`,
+
+        modalActionedId: 'confirmed-action',
+        modalActionedTitle: 'Confirmed Action',
+        modalActionedBody: 'The action has been carried out.',
+        modalActionedOnCLick: `id="confirmed-load-ok"onclick="window.mshMethods.testConfirmedDone()"`,
+        modalActionedFooterOk: true
+        // modalPublishedOk: 'OK',
+       
+    }
+
+    meth.extendMethods({
+
+        testConfirm: function () {
+            loadPair.action(options);
+        },
+        testConfirmed: function () {
+            $(`#${options.modalActionId}`).remove();
+            loadPair.actioned(options);
+        },
+        testConfirmedDone: function () {
+            $(`#${options.modalActionedId}`).remove();
+            modal.showModal('testFinalDone', 'Test Final Done', 'The final test complete');
+        }
+    });
+
+    var loadPair = new mas.PairOverlay(options);
 
     $('#show-modal').on('click', (e) => {
 
@@ -13,6 +55,15 @@
         }
 
         modal.showModal('xx1', 'Test Title', body);
+    });
+
+    $('#show-overlayl').on('click', (e) => {
+
+        e.preventDefault();
+
+        window.mshMethods.testConfirm();
+
+        
     });
 
 }(jQuery));
